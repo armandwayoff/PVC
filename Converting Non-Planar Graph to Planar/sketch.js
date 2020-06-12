33b0550c1aa2063f8601dd2af7ab40d61191634a
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-12 22:48:15 +0200

    Update sketch.js

--- Converting Non-Planar Graph to Planar/sketch.js
+++ Converting Non-Planar Graph to Planar/sketch.js
@@ -1,35 +1,57 @@
 let p = [];
-const nbrPtn = 20;
-const vitesseAffichage = 10;
+let originalP = [];
+let o = [];
+const nbrPtn = 5;
+const vitesseAffichage = 20;
+const bord = 20;
+const rayon = 10;
+
+let iter = 0;
 
 function setup() {
   createCanvas(750, 550);
   textSize(20);
+  // p[0] = new Point();
+  // for (let i = 1; i < nbrPtn; i++) {
+  //   let newPoint = new Point();
+  //   if (!overlay(newPoint, p)) {
+  //     p[i] = newPoint;
+  //   }
+  //   originalP.push(i);
+  // }
   for (let i = 0; i < nbrPtn; i++) {
     p[i] = new Point();
+    originalP.push(i);
   }
+  o = [...p];
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  for (let i = 0; i < p.length; i++) {
+    p[i].affichage();
+    noStroke();
+    text(i + 1, o[i].x + 10, o[i].y + 10);
+  }
+
+  for (let i = 0; i < p.length - 1; i++) {
+    stroke(0);
+    strokeWeight(2);
+    line(p[i].x, p[i].y, p[i + 1].x, p[i + 1].y);
+  }
+  
+  if (iter == 0) {
+   // saveCanvas();
+  }
+  
+  iter = 1;
+  
   comparaison();
 }
 
 function comparaison() {
   let nbrInter = 0;
-
-  for (let i = 0; i < p.length; i++) {
-    p[i].affichage();
-    // noStroke();
-    // text(i + 1, p[i].x + 10, p[i].y + 10);
-  }
-
-  for (let i = 0; i < p.length - 1; i++) {
-    stroke(0);
-    strokeWeight(1);
-    line(p[i].x, p[i].y, p[i + 1].x, p[i + 1].y);
-  }
-
 
   if (frameCount % vitesseAffichage == 0) {
     for (let i = 0; i < p.length - 2; i++) {
@@ -38,10 +60,15 @@ function comparaison() {
           let inter = intersection(p[i].x, p[i].y, p[i + 1].x, p[i + 1].y, p[j].x, p[j].y, p[j + 1].x, p[j + 1].y);
           if (inter) {
             swap(p, i + 1, j);
+            swap(originalP, i + 1, j);
             nbrInter++;
           }
         }
       }
+    }
+    if (nbrInter == 0) {
+      adjacencyMatrix(originalP);
+      noLoop();
     }
   }
 }
@@ -64,15 +91,45 @@ function intersection(a, b, c, d, p, q, r, s) {
   }
 }
 
+function adjacencyMatrix(arr) {
+  let matrix = [];
+  for (let i = 0; i < arr.length; i++) {
+    matrix.push([]);
+    for (let j = 0; j < arr.length; j++) {
+      matrix[i].push(0);
+    }
+  }
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    matrix[arr[i]][arr[i + 1]] = 1;
+    matrix[arr[i + 1]][arr[i]] = 1;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    print(matrix[i]);
+  }
+}
+
+
+// function overlay(obj, list) {
+//   const minDistBetweenVertices = rayon * 3;
+//   for (let i = 0; i < list.length; i++) {
+//     if (dist(obj.x, obj.y, list[i].x, list[i].y) <= minDistBetweenVertices) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
 class Point {
   constructor() {
-    this.x = random(width);
-    this.y = random(height);
+    this.x = random(bord + rayon, width - rayon - bord);
+    this.y = random(bord + rayon, height - rayon - bord);
   }
 
   affichage() {
     stroke(0);
-    strokeWeight(5);
+    strokeWeight(rayon);
     point(this.x, this.y);
   }
 }
