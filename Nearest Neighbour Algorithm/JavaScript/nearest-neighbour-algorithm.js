d33c7c00b6ba4059ec3f208dd277abbc02e769b8
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-25 22:35:50 +0200

    Update and rename Nearest Neighbour Algorithm/nearest-neighbour-algorithm.js to Nearest Neighbour Algorithm/JavaScript/nearest-neighbour-algorithm.js

--- Nearest Neighbour Algorithm/JavaScript/nearest-neighbour-algorithm.js
+++ Nearest Neighbour Algorithm/JavaScript/nearest-neighbour-algorithm.js
@@ -1,5 +1,5 @@
 const NUMBER_VERTICES = 100;
-const RAD = 5
+const RAD = 5;
 
 let vertices = [];
 let visited_vertices = [0]; // By default, the graph starts with the first vertex
@@ -11,7 +11,7 @@ function setup() {
   strokeWeight(RAD / 2);
   textSize(RAD * 2);
   for (let i = 0; i < NUMBER_VERTICES; i++) {
-    vertices[i] = new Vertex(random(width), random(height));
+    vertices[i] = new Vertex();
     vertices[i].display();
     text(i, vertices[i].x + RAD * 2, vertices[i].y + RAD * 2);
   }
@@ -32,16 +32,16 @@ function draw() {
   line(vertices[nearest_vertex].x, vertices[nearest_vertex].y, vertices[current_vertex].x, vertices[current_vertex].y);
   visited_vertices.push(nearest_vertex);
   current_vertex = nearest_vertex;
-  
+
   if (visited_vertices.length == vertices.length) {
     noLoop();
   }
 }
 
 class Vertex {
-  constructor(x_, y_) {
-    this.x = x_;
-    this.y = y_;
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
   }
 
   display() {
