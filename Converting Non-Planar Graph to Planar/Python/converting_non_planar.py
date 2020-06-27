52a7a8e7dde3c4d0a5521623e5da1cbb1afcd9b6
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-28 00:31:35 +0200

    New version

--- Converting Non-Planar Graph to Planar/Python/converting_non_planar.py
+++ Converting Non-Planar Graph to Planar/Python/converting_non_planar.py
@@ -17,24 +17,8 @@ def intersection(p1, p2, q1, q2):
         return (0 < lamb < 1) and (0 < gamma < 1)
 
 
-def calc():
-    number_intersections = 0
-    for i in range(len(vertices) - 2):
-        for j in range(i + 2, len(vertices) - 1):
-            if i != j and (i + 1) != j:
-                inter = intersection(vertices[i], vertices[i + 1], vertices[j], vertices[j + 1])
-                if inter:
-                    vertices[i + 1], vertices[j] = vertices[j], vertices[i + 1]
-                    path[i + 1], path[j] = path[j], path[i + 1]
-                    number_intersections += 1
-    if number_intersections != 0:
-        return True
-    else:
-        return False
-    
-
-number_vertices = 5
-width = height = 10  # dimension of the canvas
+number_vertices = 10
+width = height = 100  # dimension of the canvas
 
 vertices = []
 path = []
@@ -45,8 +29,17 @@ for i in range(number_vertices):
     print(i, ": (" + str(vertices[i].x), ";", str(vertices[i].y) + ")")
     path.append(i)  # the vertices are initially linked in their order of generation
 
-while calc():
-    calc()
+number_intersections = 1  # suppose there is initially at least one intersection
+while number_intersections > 0:
+    number_intersections = 0
+    for i in range(len(vertices) - 2):
+        for j in range(i + 2, len(vertices) - 1):
+            if i != j and (i + 1) != j:
+                inter = intersection(vertices[i], vertices[i + 1], vertices[j], vertices[j + 1])
+                if inter:
+                    vertices[i + 1], vertices[j] = vertices[j], vertices[i + 1]  # swap the two vertices
+                    path[i + 1], path[j] = path[j], path[i + 1]
+                    number_intersections += 1
 
 print("Path :", path)
 print("Adjacency matrix :")
@@ -56,4 +49,3 @@ for i in range(number_vertices - 1):
     adjacency_matrix[path[i + 1]][path[i]] = 1
 for row in adjacency_matrix:
     print(*row)
-    
