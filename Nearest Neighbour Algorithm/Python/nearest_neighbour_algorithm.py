fa7c5d8d7f21877637f2f061865c11e5139387aa
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-03 12:13:11 +0200

    Update nearest_neighbour_algorithm.py

--- Nearest Neighbour Algorithm/Python/nearest_neighbour_algorithm.py
+++ Nearest Neighbour Algorithm/Python/nearest_neighbour_algorithm.py
@@ -12,22 +12,24 @@ def dist(x1, y1, x2, y2):
     return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
-number_vertices = 10
-width = height = 100  # dimension of the canvas
+NUMBER_VERTICES = 10
+WIDTH = HEIGHT = 100  # dimension of the canvas
 
 vertices = []
 visited_vertices = [0]  # by default, the graph starts with the first vertex
 path = [0]
 current_vertex = 0
 
+print("* Nearest Neighbour Algorithm *")
+print("Number of vertices :", NUMBER_VERTICES, "| Dimensions of the canvas : (" + str(WIDTH), ";", str(HEIGHT) + ")\n")
 print("Vertices coordinates :")
-for i in range(number_vertices):
-    vertices.append(Vertex(randint(0, width), randint(0, height)))
+for i in range(NUMBER_VERTICES):
+    vertices.append(Vertex(randint(0, WIDTH), randint(0, HEIGHT)))
     print(i, ": (" + str(vertices[i].x), ";", str(vertices[i].y) + ")")
 
 while len(visited_vertices) < len(vertices):
     global nearest_vertex
-    record_distance = dist(0, 0, width, height)
+    record_distance = dist(0, 0, WIDTH, HEIGHT)
     for i in range(len(vertices)):
         if i not in visited_vertices:
             d = dist(vertices[i].x, vertices[i].y, vertices[current_vertex].x, vertices[current_vertex].y)
@@ -40,9 +42,10 @@ while len(visited_vertices) < len(vertices):
 
 print("Path :", path)
 print("Adjacency matrix :")
-adjacency_matrix = [[0 for col in range(number_vertices)] for row in range(number_vertices)]
-for i in range(number_vertices - 1):
+adjacency_matrix = [[0 for col in range(NUMBER_VERTICES)] for row in range(NUMBER_VERTICES)]
+for i in range(NUMBER_VERTICES - 1):
     adjacency_matrix[path[i]][path[i + 1]] = 1
     adjacency_matrix[path[i + 1]][path[i]] = 1
 for row in adjacency_matrix:
     print(*row)
+    
