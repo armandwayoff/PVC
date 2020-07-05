2772e86a527964ff1f3813290aa642b7b647ffd4
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-06 00:12:01 +0200

    Add networkx graph

--- Converting Non-Planar Graph to Planar/Python/converting_non_planar.py
+++ Converting Non-Planar Graph to Planar/Python/converting_non_planar.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import networkx as nx
 from random import randint
 
 
@@ -17,17 +19,25 @@ def intersection(p1, p2, q1, q2):
         return (0 < lamb < 1) and (0 < gamma < 1)
 
 
-NUMBER_VERTICES = 10
+NUMBER_VERTICES = 100
 WIDTH = HEIGHT = 100  # dimensions of the canvas
+VERTEX_SIZE = 150
 
 vertices = []
 path = []
+
+adjacency_matrix = [[0 for col in range(NUMBER_VERTICES)] for row in range(NUMBER_VERTICES)]
+
+G = nx.Graph()
+
 print("* Converting Non-Planar Graph to Planar *")
 print("Number of vertices :", NUMBER_VERTICES, "| Dimensions of the canvas : (" + str(WIDTH), ";", str(HEIGHT) + ")\n")
 print("Vertices coordinates :")
 for i in range(NUMBER_VERTICES):
-    vertices.append(Vertex(randint(0, WIDTH), randint(0, HEIGHT)))
-    print(i, ": (" + str(vertices[i].x), ";", str(vertices[i].y) + ")")
+    new_vertex = Vertex(randint(0, WIDTH), randint(0, HEIGHT))
+    vertices.append(new_vertex)
+    G.add_node(i, pos=(new_vertex.x, new_vertex.y))
+    print(i, ": (" + str(new_vertex.x), ";", str(new_vertex.y) + ")")
     path.append(i)  # the vertices are initially linked in their order of generation
 
 number_intersections = 1  # suppose there is initially at least one intersection
@@ -44,9 +54,13 @@ while number_intersections > 0:
 
 print("Path :", path)
 print("Adjacency matrix :")
-adjacency_matrix = [[0 for col in range(NUMBER_VERTICES)] for row in range(NUMBER_VERTICES)]
 for i in range(NUMBER_VERTICES - 1):
     adjacency_matrix[path[i]][path[i + 1]] = 1
     adjacency_matrix[path[i + 1]][path[i]] = 1
+    G.add_edge(path[i], path[i + 1])
 for row in adjacency_matrix:
     print(*row)
+
+pos = nx.get_node_attributes(G, 'pos')
+nx.draw(G, pos, node_size=VERTEX_SIZE, node_color='orange', with_labels=True)
+plt.show()
