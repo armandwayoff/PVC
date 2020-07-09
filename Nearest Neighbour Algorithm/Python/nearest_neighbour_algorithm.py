f2e98f022227e73b1e6f509b4991b09db16493a4
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-09 22:42:31 +0200

    Add adjacency_matrix

--- Nearest Neighbour Algorithm/Python/nearest_neighbour_algorithm.py
+++ Nearest Neighbour Algorithm/Python/nearest_neighbour_algorithm.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from math import sqrt
 from random import randint
+from itertools import permutations
 
 
 class Vertex:
@@ -14,20 +15,24 @@ def dist(x1, y1, x2, y2):
     return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
-NUMBER_VERTICES = 100
+def totalDist(lst):
+    d = 0
+    for j in range(len(lst) - 1):
+        d += dist(vertices[lst[j]].x, vertices[lst[j]].y, vertices[lst[j + 1]].x, vertices[lst[j + 1]].y)
+    return d
+
+
+NUMBER_VERTICES = 6
 WIDTH = HEIGHT = 100  # dimension of the canvas
 VERTEX_SIZE = 150
 
 vertices = []
-visited_vertices = [0]  # by default, the graph starts with the first vertex
-path = [0]
-current_vertex = 0
 
 adjacency_matrix = [[0 for col in range(NUMBER_VERTICES)] for row in range(NUMBER_VERTICES)]
 
 G = nx.Graph()
 
-print("* Nearest Neighbour Algorithm *")
+print("* Brute Force Algorithm *")
 print("Number of vertices :", NUMBER_VERTICES, "| Dimensions of the canvas : (" + str(WIDTH), ";", str(HEIGHT) + ")\n")
 print("Vertices coordinates :")
 for i in range(NUMBER_VERTICES):
@@ -36,25 +41,22 @@ for i in range(NUMBER_VERTICES):
     G.add_node(i, pos=(new_vertex.x, new_vertex.y))
     print(i, ": (" + str(vertices[i].x), ";", str(vertices[i].y) + ")")
 
-while len(visited_vertices) < len(vertices):
-    global nearest_vertex
-    record_distance = dist(0, 0, WIDTH, HEIGHT)
-    for i in range(len(vertices)):
-        if i not in visited_vertices:
-            d = dist(vertices[i].x, vertices[i].y, vertices[current_vertex].x, vertices[current_vertex].y)
-            if d < record_distance:
-                nearest_vertex = i
-                record_distance = d
-    G.add_edge(nearest_vertex, current_vertex)
-    visited_vertices.append(nearest_vertex)
-    path.append(nearest_vertex)
-    current_vertex = nearest_vertex
+orders = permutations(range(NUMBER_VERTICES))
+record_distance = dist(0, 0, WIDTH, HEIGHT) * NUMBER_VERTICES
+global record_path
+for o in list(orders):
+    if totalDist(o) < record_distance:
+        record_distance = totalDist(o)
+        record_path = o
 
-print("Path :", path)
+print("Shortest Path :", record_path)
+
+for v in range(len(record_path) - 1):
+    G.add_edge(record_path[v], record_path[v + 1])
+    adjacency_matrix[record_path[v]][record_path[v + 1]] = 1
+    adjacency_matrix[record_path[v + 1]][record_path[v]] = 1
+
 print("Adjacency matrix :")
-for i in range(NUMBER_VERTICES - 1):
-    adjacency_matrix[path[i]][path[i + 1]] = 1
-    adjacency_matrix[path[i + 1]][path[i]] = 1
 for row in adjacency_matrix:
     print(*row)
 
