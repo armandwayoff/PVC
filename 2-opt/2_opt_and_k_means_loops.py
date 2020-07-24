6af3326b29ef3cd9f2b3edae18cca4c69621f5e2
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-07-24 10:28:34 +0200

    new version

--- 2-opt/2_opt_and_k_means_loops.py
+++ 2-opt/2_opt_and_k_means_loops.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import time
 
+
 class Vertex:
     def __init__(self, x, y):
         self.x = x
@@ -19,19 +20,21 @@ class Centroid:
 def dist(x1, y1, x2, y2):
     return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
+
 # cluster's functions
+
 
 def create_clusters(reference_elements, elements_to_organise):
     global target_index
     new_node_color = []
     new_clusters = [[] for _ in range(NUMBER_CLUSTERS)]  # initialisation of the clusters list
     for k in range(len(elements_to_organise)):
-        record_distance = dist(0, 0, WIDTH, HEIGHT)
+        record = dist(0, 0, WIDTH, HEIGHT)
         for j in range(len(reference_elements)):
             d = dist(elements_to_organise[k].x, elements_to_organise[k].y,
                      reference_elements[j].x, reference_elements[j].y)
-            if d < record_distance:
-                record_distance = d
+            if d < record:
+                record = d
                 target_index = j
         new_clusters[target_index].append(elements_to_organise[k])
         new_node_color.append(COLORS[target_index])
@@ -45,7 +48,9 @@ def centroid_of(lst):
         yG += lst[a].y / len(lst)
     return Centroid(xG, yG)
 
+
 # graph's functions
+
 
 def total_distance(lst):
     d = 0
@@ -59,8 +64,8 @@ def reverse_sublist(lst, start, end):
     return lst
 
 
-NUMBER_VERTICES = 50
-NUMBER_CLUSTERS = 5
+NUMBER_VERTICES = 30
+NUMBER_CLUSTERS = 8  # up to 8
 NUMBER_ITERATIONS = 10 ** 4
 WIDTH = HEIGHT = 100  # dimension of the canvas
 VERTEX_SIZE = 150
@@ -69,19 +74,17 @@ COLORS = ['orange', 'red', 'purple', 'green', 'black', 'grey', 'pink', 'cyan']
 vertices = []
 G = nx.Graph()
 
-print("* K-means *")
+print("* 2-opt & K-means *")
 print("Number of vertices :", NUMBER_VERTICES,
       "| Number of clusters :", NUMBER_CLUSTERS,
       "| Dimensions of the canvas : (" + str(WIDTH), ";", str(HEIGHT) + ")\n")
 
 start_time = time.time()
 # creation of the vertices
-# print("Vertices coordinates :")
 for i in range(NUMBER_VERTICES):
     new_vertex = Vertex(randint(1, WIDTH), randint(1, HEIGHT))
     vertices.append(new_vertex)
     G.add_node(i, pos=(new_vertex.x, new_vertex.y))
-    # print(i, ": (" + str(vertices[i].x), ";", str(vertices[i].y) + ")")
 pos = nx.get_node_attributes(G, 'pos')
 
 # initialisation
@@ -102,30 +105,33 @@ while previous_state != current_state:
     clusters, node_color = create_clusters(centroids, vertices)
     current_state = clusters
     iteration += 1
-print("Clusters : done")
-# --------------------------------------------------------------
+print("Clusters : ✓")
 print("--- %s seconds ---" % (time.time() - start_time))
+# --------------------------------------------------------------
 
 # graphs
 # --------------------------------------------------------------
 for cluster in clusters:
-    path = [vertices.index(vertex) for vertex in cluster]  # initial path
-    path.append(path[0])  # creation of the loop
-    record_distance = dist(0, 0, WIDTH, HEIGHT) * NUMBER_VERTICES
-    for i in range(NUMBER_ITERATIONS):
-        selected_vertices = sample(range(1, len(path) - 1), 2)
-        test = path.copy()
-        test = reverse_sublist(test, selected_vertices[0], selected_vertices[1])
-        test_distance = total_distance(test)
-        if test_distance < record_distance:
-            record_distance = test_distance
-            path = test
-    for i in range(len(cluster)):
-        G.add_edge(path[i], path[i + 1])
-print("Tours : done")
-# --------------------------------------------------------------
+    if len(cluster) > 2:
+        path = [vertices.index(vertex) for vertex in cluster]  # initial path
+        path.append(path[0])  # creation of the loop
+        record_distance = dist(0, 0, WIDTH, HEIGHT) * NUMBER_VERTICES
+        for i in range(NUMBER_ITERATIONS):
+            selected_vertices = sample(range(1, len(path) - 1), 2)
+            test = path.copy()
+            test = reverse_sublist(test, selected_vertices[0], selected_vertices[1])
+            test_distance = total_distance(test)
+            if test_distance < record_distance:
+                record_distance = test_distance
+                path = test
+        for i in range(len(cluster)):
+            G.add_edge(path[i], path[i + 1])
+    if len(cluster) == 2:
+        G.add_edge(vertices.index(cluster[0]), vertices.index(cluster[1]))
 
+print("Graphs : ✓")
 print("--- %s seconds ---" % (time.time() - start_time))
+# --------------------------------------------------------------
 
 plt.figure(str(NUMBER_CLUSTERS) + "-means | Iteration " + str(iteration))
 nx.draw(G, pos, node_size=VERTEX_SIZE, node_color=node_color, with_labels=True)
