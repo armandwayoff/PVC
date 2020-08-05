0dbdccf6d9058873ed4ad20f1518e02e8788a1ec
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-05 20:58:59 +0200

    Create simulated_annealing.py

---
+++ Simulated Annealing/simulated_annealing.py
@@ -0,0 +1,81 @@
+# Thanks to this video https://www.youtube.com/watch?v=35fzyblVdmA&t=578s
+
+import matplotlib.pyplot as plt
+import networkx as nx
+from math import *
+from random import *
+import time
+
+
+class Vertex:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def dist(x1, y1, x2, y2):
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def total_distance(lst):
+    d = 0
+    for j in range(len(lst) - 1):
+        d += dist(vertices[lst[j]].x, vertices[lst[j]].y, vertices[lst[j + 1]].x, vertices[lst[j + 1]].y)
+    return d
+
+
+def reverse_sublist(lst, start, end):
+    lst[start:end + 1] = lst[start:end + 1][::-1]
+    return lst
+
+
+NUMBER_VERTICES = 40
+WIDTH = HEIGHT = 100  # dimension of the canvas
+NUMBER_ITERATIONS = 10 ** 3
+NUMBER_ITERATIONS_PER_CHAIN = 500
+VERTEX_SIZE = 150
+
+INITIAL_TEMP = 50
+ALPHA = 0.99
+T = INITIAL_TEMP
+
+vertices = []
+path = []
+
+G = nx.Graph()
+
+for i in range(NUMBER_VERTICES):
+    new_vertex = Vertex(randint(0, WIDTH), randint(0, HEIGHT))
+    vertices.append(new_vertex)
+    G.add_node(i, pos=(new_vertex.x, new_vertex.y))
+    path.append(i)
+path.append(0)
+
+start_time = time.time()
+
+record_distance = dist(0, 0, WIDTH, HEIGHT) * NUMBER_VERTICES
+for _ in range(NUMBER_ITERATIONS):
+    T *= ALPHA
+    for _ in range(NUMBER_ITERATIONS_PER_CHAIN):
+        selected_vertices = sample(range(1, NUMBER_VERTICES), 2)
+        test = path.copy()
+        test = reverse_sublist(test, selected_vertices[0], selected_vertices[1])
+        test_distance = total_distance(test)
+        if test_distance < record_distance:
+            record_distance = test_distance
+            path = test
+        else:
+            x = uniform(0, 1)
+            if x < exp((record_distance - test_distance) / T):
+                record_distance = test_distance
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+for i in range(NUMBER_VERTICES):
+    G.add_edge(path[i], path[i + 1])
+
+plt.figure("Graph")
+pos = nx.get_node_attributes(G, 'pos')
+nx.draw(G, pos, node_size=VERTEX_SIZE, node_color='orange', with_labels=True)
+
+plt.show()
