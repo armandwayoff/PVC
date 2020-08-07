2edaae743fb85ed29a5f071af1b2d81c7aba519c
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-07 16:54:36 +0200

    Create NN_function.py

---
+++ Nearest Neighbour Algorithm/NN_function.py
@@ -0,0 +1,25 @@
+import math
+
+
+def nearest_neighbour(population, starting_vertex):
+
+    def dist(v1, v2):
+        return math.sqrt((v1.x - v2.x) ** 2 + (v1.y - v2.y) ** 2)
+
+    visited_vertices = [population[starting_vertex]]
+    path = [population[starting_vertex]]
+    current_vertex = population[starting_vertex]
+    nearest_vertex = current_vertex
+    
+    while len(visited_vertices) < len(population):
+        record_distance = float('inf')
+        for vertex in population:
+            if vertex not in visited_vertices:
+                d = dist(vertex, current_vertex)
+                if d < record_distance:
+                    nearest_vertex = vertex
+                    record_distance = d
+        visited_vertices.append(nearest_vertex)
+        path.append(nearest_vertex)
+        current_vertex = nearest_vertex
+    return path
