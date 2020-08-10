b7b795284f701255b06951205301d1ae04e000fc
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-10 22:42:56 +0200

    cleaner version

--- Nearest Neighbour Algorithm/NN_function.py
+++ Nearest Neighbour Algorithm/NN_function.py
@@ -1,16 +1,16 @@
-import math
+from math import *
 
 
 def nearest_neighbour(population, starting_vertex):
-
+    
     def dist(v1, v2):
-        return math.sqrt((v1.x - v2.x) ** 2 + (v1.y - v2.y) ** 2)
+        return sqrt((v1.x - v2.x) ** 2 + (v1.y - v2.y) ** 2)
 
     visited_vertices = [population[starting_vertex]]
     path = [population[starting_vertex]]
     current_vertex = population[starting_vertex]
     nearest_vertex = current_vertex
-    
+
     while len(visited_vertices) < len(population):
         record_distance = float('inf')
         for vertex in population:
