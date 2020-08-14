7fc98e26b8707bcfb276fc539cb1600cdf485da3
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-15 01:23:30 +0200

    placement of the plot

--- GUI/main_structure.py
+++ GUI/main_structure.py
@@ -6,20 +6,18 @@ root = Tk()
 root.geometry("800x600")
 root.title("TSP Solver")
 
-graph_frame = Frame(root, width=600, height=500, bg="red").pack(side=LEFT, fill=BOTH, expand=TRUE)
 
 def plot():
-    fig = Figure(figsize=(5, 5),
-                 dpi=100)
+    fig = Figure(figsize=(5, 5), dpi=100)
     y = [i ** 2 for i in range(101)]
     plot1 = fig.add_subplot(111)
     plot1.plot(y)
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
-    canvas.get_tk_widget().pack()
-    toolbar = NavigationToolbar2Tk(canvas, root)
-    toolbar.update()
-    canvas.get_tk_widget().pack()
+    # canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=TRUE)
+    # toolbar = NavigationToolbar2Tk(canvas, root)
+    # toolbar.update()
+    canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=TRUE)
 
 
 parameters_frame = Frame(root, width=200, height=500, bg="blue").pack(side="right", fill=BOTH, expand=TRUE)
