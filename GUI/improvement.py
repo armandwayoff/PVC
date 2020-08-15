b8217ae0a821e396e41817c98e3b0d3250118cea
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-15 14:47:48 +0200

    Create improvement.py

---
+++ GUI/improvement.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from random import *
+
+root = Tk()
+root.title("TSP Solver")
+root.geometry("800x600")
+root.minsize(800, 600)
+
+
+def new_random_plot():
+    fig = Figure(figsize=(5, 5), dpi=100)
+    x = [uniform(0, 10) for _ in range(50)]
+    y = [uniform(0, 10) for _ in range(50)]
+    new_plot = fig.add_subplot(111)
+    new_plot.scatter(x, y, c="black")
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    # canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=TRUE)
+    # toolbar = NavigationToolbar2Tk(canvas, root)
+    # toolbar.update()
+    canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=TRUE)
+
+
+label_frame = LabelFrame(root, text="Parameters", width=300, height=800).pack(side=RIGHT, expand='no', fill='y')
+button = Button(label_frame, text="NEW", command=new_random_plot()).pack()
+
+mainloop()
