40d7618ed384d9ea83ba207e849d80478651165d
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-16 18:30:59 +0200

    it goes well !

--- GUI/main.py
+++ GUI/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter.ttk import Combobox
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -6,8 +7,11 @@ from random import *
 
 root = Tk()
 root.title("TSP Solver")
+root.iconbitmap('icon.ico')
 root.geometry("800x600")
 root.minsize(800, 600)
+
+global x, y
 
 
 def new_random_plot():
@@ -18,32 +22,47 @@ def new_random_plot():
     canvas.draw()
 
 
-# Parameters
+def solve():
+    # Initialisation process
+    if initialisation_algorithm.get() == "Random":
+        print(1)
+    elif initialisation_algorithm.get() == "Nearest neighbour algorithm":
+        print(2)
+    else:
+        messagebox.showerror("Error", "The selected initialization algorithm is not valid")
 
+
+# Parameters
 parameters_frame = LabelFrame(root, text="Parameters", width=300, height=800, bg='white')
 parameters_frame.pack(side=RIGHT, expand=0, fill=Y)
 
 # Data
-label1 = Label(parameters_frame, text="Data", bg="white").pack()
+data_label = Label(parameters_frame, text="Data", bg="white").pack()
 import_data_button = Button(parameters_frame, text="Import data").pack()
 new_random_plot_button = Button(parameters_frame, text="New random plot", command=new_random_plot).pack()
 
-# Initialisation algorithm
-label2 = Label(parameters_frame, text="Initialisation algorithm", bg="white").pack()
-initialisation_algorithms = ["Random", "Nearest neighbour algorithm"]
-selection = Combobox(parameters_frame, values=initialisation_algorithms)
-selection.current(0)
-selection.pack()
+# Initialisation algorithms
+initialisation_algorithms_label = Label(parameters_frame, text="Initialisation algorithm", bg="white").pack()
+initialisation_algorithms_lst = ["Random", "Nearest neighbour algorithm"]
+initialisation_algorithm = StringVar()
+initialisation_algorithms_combobox = Combobox(parameters_frame,
+                                              values=initialisation_algorithms_lst,
+                                              textvariable=initialisation_algorithm)
+initialisation_algorithms_combobox.current(0)
+initialisation_algorithms_combobox.pack()
 
-# Solving algorithm
-label3 = Label(parameters_frame, text="Solving algorithm", bg="white").pack()
-solving_algorithms = ["2-opt", "Simulated annealing", "Brute-force search"]
-selection1 = Combobox(parameters_frame, values=solving_algorithms)
-selection1.current(0)
-selection1.pack()
+# Solving algorithms
+solving_algorithms_label = Label(parameters_frame, text="Solving algorithm", bg="white").pack()
+solving_algorithms_lst = ["2-opt", "Simulated annealing", "Brute-force search"]
+solving_algorithm = StringVar()
+solving_algorithms_combobox = Combobox(parameters_frame,
+                                       values=solving_algorithms_lst,
+                                       textvariable=solving_algorithm)
+solving_algorithms_combobox.current(0)
+solving_algorithms_combobox.pack()
 
 # Solve button
-solve_button = Button(parameters_frame, text="Solve", bg='limegreen').pack()
+solve_button = Button(parameters_frame, text="Solve", bg='limegreen', command=solve).pack()
 
 # Graph
 
