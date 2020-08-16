a24b5aa41985aca4f38a8af646aae0481e029608
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-16 10:22:37 +0200

    add the toolbar

--- GUI/improvement.py
+++ GUI/improvement.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from random import *
 
 root = Tk()
@@ -11,11 +11,13 @@ root.minsize(800, 600)
 fig = Figure(figsize=(5, 5), dpi=100)
 new_plot = fig.add_subplot(111)
 canvas = FigureCanvasTkAgg(fig, master=root)
+toolbar = NavigationToolbar2Tk(canvas, root)
+toolbar.update()
 
 
 def new_random_plot():
-    x = [randint(0, 10) for _ in range(25)]
-    y = [randint(0, 10) for _ in range(25)]
+    x = [uniform(0, 10) for _ in range(50)]
+    y = [uniform(0, 10) for _ in range(50)]
     new_plot.clear()
     new_plot.scatter(x, y, c="black")
     canvas.draw()
