9442c032231b0a17a7fba4164a1e792974113538
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-21 16:34:50 +0200

    Create read_coords_from_file.py

---
+++ GUI/read_coords_from_file.py
@@ -0,0 +1,14 @@
+"""
+Format : 
+x0 y0
+x1 y1
+.....
+xN yN
+"""
+
+vertices = []
+file = open("file.txt", "r")
+for line in file:
+    coords = line[:-1].split(" ")
+    vertices.append((int(coords[0]), int(coords[1])))
+print(vertices)
